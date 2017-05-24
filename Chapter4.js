3ab7d403220945edac185dc98910e542cf4616f2
Hewwenxi <e0146877@u.nus.edu>
2017-05-24 23:11:03 +0800

    Chapter4Following

---
+++ Chapter4.js
@@ -0,0 +1,57 @@
+// var listOfNumbers = [2, 3, 5, 11];
+// console.log(listOfNumbers[0]);
+
+////////////////////////////////////////////////////////
+
+// array
+
+// var doh = "Doh";
+// console.log(doh.toUpperCase());
+// console.log(doh.toLowerCase());
+
+// var mack = [];
+// mack.push("Mack");
+// mack.push("the", "Knife");
+// console.log(mack);
+// console.log(mack.join(" "));
+// console.log(mack.pop());
+// console.log(mack);
+
+////////////////////////////////////////////////////////
+
+// // object
+
+// var day1 = {
+// 	squirrel: false,
+// 	events: ["works", "touched tree", "pizza", "running", "television"] 
+// };
+
+// console.log(day1.squirrel);
+// console.log(day1.wolf);
+// day1.wolf = false;
+// console.log(day1.wolf);
+
+////////////////////////////////////////////////////////
+
+// var descriptions = {
+// 	work: "Went to work",
+// 	"touched tree": "Touched a tree"
+// };
+
+// console.log(descriptions.work);
+// console.log(descriptions["touched tree"]);
+
+////////////////////////////////////////////////////////
+
+// var anObject = {
+// 	left: 1,
+// 	right: 2
+// };
+
+// console.log(anObject.left);
+// delete anObject.left;
+// console.log(anObject.left);
+// console.log("left" in anObject);
+// console.log("right" in anObject);
+
+////////////////////////////////////////////////////////
